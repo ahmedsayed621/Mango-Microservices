5429fc19db3c31460405fd727f4489e7dcfd7d0a
ahmedsayed621 <ahmedsayed621@yahoo.com>
2022-12-22 07:47:12 +0200

    Add project files.

---
+++ ProductService.API/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductService.API.Models;
+
+namespace ProductService.API.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
